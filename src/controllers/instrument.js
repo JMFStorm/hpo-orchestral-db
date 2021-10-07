78b32e402490d7947ea37bb3ebbf736a6112edad
Janne Fagerström <jannematiasf@gmail.com>
2021-10-08 00:11:27 +0300

    add csv reader test

--- src/controllers/instrument.js
+++ src/controllers/instrument.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
+const Papa = require("papaparse");
 
 const httpError = require("../utils/httpError");
 const { getAllInstruments } = require("../managers/instrument");
 
-const instrumentController = Router();
+// Make env var!
+const csvFilePath = "../csv/hpo_test_data.csv";
 
-// Get all instruments
-instrumentController.get("/", async (req, res, next) => {
+const router = Router();
+
+router.get("/", async (req, res, next) => {
   try {
     const response = await getAllInstruments();
     res.send(response);
@@ -14,5 +17,32 @@ instrumentController.get("/", async (req, res, next) => {
     return next(httpError(err, 404));
   }
 });
+// Get test data
+router.get("/data", async (req, res, next) => {
+  try {
+    const fs = require("fs");
+    const csvR = require("jquery-csv");
+    const sample = "../csv/hpo_test_data.csv";
 
-module.exports = instrumentController;
+    fs.readFile(sample, "latin1", function (err, csv) {
+      if (err) {
+        console.log(err);
+      }
+      csvR.toArrays(csv, {}, function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        console.log("data", data);
+        for (var i = 0, len = data.length; i < len; i++) {
+          console.log(data[i]);
+        }
+      });
+    });
+    res.send("");
+  } catch (err) {
+    console.error("err", err);
+    return next(httpError(err, 404));
+  }
+});
+
+module.exports = router;
