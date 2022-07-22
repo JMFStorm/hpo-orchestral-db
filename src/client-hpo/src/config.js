6b48dc29cead42055663216c7b1cd8212dfeac71
JMFStorm <jannematiasf@gmail.com>
2022-07-22 18:15:25 +0300

    PROD

--- src/client-hpo/src/config.js
+++ src/client-hpo/src/config.js
@@ -1,4 +1,2 @@
 export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_SERVER_URL_DEMO
-    : process.env.REACT_APP_SERVER_URL_DEV;
+  process.env.NODE_ENV === "production" ? process.env.REACT_APP_SERVER_URL_PROD : process.env.REACT_APP_SERVER_URL_DEV;
